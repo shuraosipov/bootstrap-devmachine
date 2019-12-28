3d578e73e776d6fccd1ca94b8a4fdf38e145f374
Alexander Osipov <shuraosipov@gmail.com>
2019-12-27 23:28:45 -0500

    install anaconda, npm and cdk; added readme

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # bootstrap-devmachine
 Provision and configure development environment on AWS EC2 instance
+
+1. Deploy the Devmachine
+`terraform apply`
+2. When finished you will see ip address of the Devmachine
+`ip_address = 54.174.190.232`
+3. Connect to it using your key: `ssh -i ~/shuraosipov-dev-machine.pem ec2-user@54.174.190.232`
+4. Go to an app folder: `cd myapp`
+5. `source .env/bin/activate`
+6. `pip install -r requirements.txt`
+7. Check that all requirements are met and cdk works fine: `cdk ls`
+8. Deploy the app: `cdk deploy`
--- install.sh
+++ install.sh
@@ -1,55 +1,97 @@
 #!/usr/bin/bash
 # Install Development toools
-# sudo yum -y groupinstall 'Development tools'
-sudo yum -y install wget curl
+sudo yum -y groupinstall 'Development tools'
 
 # Install and configure docker
-sudo yum update -y
-sudo amazon-linux-extras install docker -y
-sudo systemctl enable docker
-sudo systemctl start docker
-sudo usermod -aG docker ec2-user
 
-# Download miniconda and install python
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
-bash ~/miniconda.sh -b -p "$HOME"/miniconda
-~/miniconda/bin/conda create -y -n dev python=3.7 anaconda
-~/miniconda/bin/conda activate dev
+#sudo yum update -y
+#sudo amazon-linux-extras install docker -y
+#sudo systemctl enable docker
+#sudo systemctl start docker
+#sudo usermod -aG docker ec2-user
+## Download miniconda and install python
+#
+# ++++++++++++++++++++ START ANACONDA INSTALL +++++++++++++++++++++
 
-# Install aws cli
-pip install awscli --upgrade --user
+# Download the Linux Anaconda Distribution
+wget https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh -O /tmp/anaconda3.sh
+
+# Run the installer (installing without -p should automatically install into '/' (root dir)
+bash /tmp/anaconda3.sh -b -p /home/ec2-user/anaconda3
+rm /tmp/anaconda3.sh
+
+### Run the conda init script to setup the shell
+echo ". /home/ec2-user/anaconda3/etc/profile.d/conda.sh" >> /home/ec2-user/.bashrc
+. /home/ec2-user/anaconda3/etc/profile.d/conda.sh
+source /home/ec2-user/.bashrc
+
+# Create a base Python3 environment separate from the base env
+conda create -y --name dev python=3.7
+conda activate dev
+conda install -y -c conda-forge awscli
+
+
 
 # Install node and npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
-nvm install node
+nvm install 12
+nvm use 12
 
 # Install aws cdk
-sudo npm install -g aws-cdk
+mkdir myapp && cd myapp
+npm install -g aws-cdk
+cdk init --language python sample-app
+source .env/bin/activate
+pip install -r requirements.txt
+pip install --upgrade aws-cdk.core
 
-
-# Donwload and install terraform
-wget https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip
-unzip terraform_0.11.7_linux_amd64.zip
-sudo ln -s ~/terraform /usr/bin/terraform
-
-
-
-
-
-
-
-# Install aws sam
-#sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-#test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
-#test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-#test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
-#echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
-#brew tap aws/tap
-#brew install aws-sam-cli
-
-
-
-
-# Install and run Jupiter Server
-
+#
+#
+## Install aws cli
+#pip install awscli --upgrade --user
+#conda activate dev
+#conda install -y -c conda-forge awscli
+#
+## Install node and npm
+#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+#. ~/.nvm/nvm.sh
+#nvm install node npm
+## Install aws cdk
+#npm install -g aws-cdk
+#
+##59  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+##   60  . ~/.nvm/nvm.sh
+##   61  nvm install node npm
+##   62  sudo -E npm install -g aws-cdk
+##   63  sudo -e npm install -g aws-cdk
+##   64  npm install -g aws-cdk
+#
+#
+#
+#
+## Donwload and install terraform
+##wget https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip
+##unzip terraform_0.11.7_linux_amd64.zip
+##sudo ln -s ~/terraform /usr/bin/terraform
+#
+#
+#
+#
+#
+#
+#
+## Install aws sam
+##sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+##test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+##test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+##test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+##echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+##brew tap aws/tap
+##brew install aws-sam-cli
+#
+#
+#
+#
+## Install and run Jupiter Server
+#
